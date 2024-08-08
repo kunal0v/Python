d91cae1d60ae26812f6d2ba62c5f7b93197d6172
kunal0v <bhandarikunal26@gmail.com>
2024-08-08 16:48:50 +0530

    empty Pyramic

--- lists and tuple/squarebox.py
+++
@@ -1,8 +0,0 @@
-x=int(input("Enter the value "))
-for i in range(1,x+1):
-    for j in range(1,x+1):
-        if(x!=2 or x!=(x+1)):
-            print("*",end="")
-        print("")
-            print("*","","*")
-        else:
