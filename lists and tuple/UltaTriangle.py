796c397e76527ffa862b61fb8893130cd1bc8643
kunal0v <bhandarikunal26@gmail.com>
2024-08-08 17:09:14 +0530

    Kaju_katli Code

---
+++ lists and tuple/UltaTriangle.py
@@ -0,0 +1,10 @@
+x=int(input("Enter the value:"))
+for i in range (x,0,-1):
+    for j in range(1,x-i+1):
+        print(" ",end="")
+    for k in range(1,2*i):
+        if(i==1 or i==x or k==1 or k==2*i-1):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
