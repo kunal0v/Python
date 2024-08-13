dfc01bfeda2961e8102d6cd4a2c1fcae4329f33e
kunal0v <bhandarikunal26@gmail.com>
2024-08-13 17:13:56 +0530

    string program

---
+++ lists and tuple/AllBout_String.py
@@ -0,0 +1,32 @@
+x=input("Enter Youer name:")
+#lower conversion
+print(x.lower())
+
+# uppper conversion
+y=input("Enter Your Friend Name:")
+print(y.upper())
+
+# to capitalize
+print(x.capitalize())
+# to count
+print(x.count("u"))
+
+# to find
+y=x.find("z")
+print(y)
+# to find the index
+z=x.index("u")
+print(z)
+# for equal spaces
+a=x.center(50)
+print(a)
+
+# to check it is alphabet and number
+print(x.isalnum())
+
+# to check it is alphabet
+print(x.isalpha())
+
+# to check it is decimal
+d="8656"
+print(d.isdecimal())
---
+++ lists and tuple/prgrm.py
@@ -0,0 +1,10 @@
+x="Hello i  am world!"
+count=1
+for i in range(len(x)):
+    if(x[i]==" "):
+        if(x[i]==" " and x[i-1]==" "):
+            continue
+        else:
+            count+=1
+
+print(count)
