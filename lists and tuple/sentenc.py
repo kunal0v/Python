b8e5c68abc8fa93a99e4b401a07d6305b29cd298
kunal0v <bhandarikunal26@gmail.com>
2024-08-26 21:28:43 +0530

    no. of words in a sentence

---
+++ lists and tuple/sentenc.py
@@ -0,0 +1,14 @@
+x = input("Enter the sentence: ")
+count = 0
+ch = False
+
+for i in range(len(x)):
+    if x[i] != " ":
+        if not ch:
+            count += 1
+            ch = True
+    else:
+        
+        ch = False
+
+print(count)
