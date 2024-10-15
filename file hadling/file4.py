697f1ae55b107d922fd7bc8b820c03f27e49070b
Kunal Singh Bhandari <bhandarikunal26@gmail.com>
2024-10-15 16:58:51 +0530

    file handling

---
+++ file hadling/file4.py
@@ -0,0 +1,11 @@
+with open("file4.txt","w+") as file:
+    file.write("Hello Happy Birthday")
+    file.seek(0)
+    file.writelines(["Ramu","shyamu\n"])
+
+with open("file4.txt","r") as file:
+    print(file.read())
+    file.seek(0)
+    print(file.readline())
+    file.seek(0)
+    print(file.readlines())
